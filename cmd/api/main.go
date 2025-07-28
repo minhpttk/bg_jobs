8102d5d6fc721cbf2000e53e66a0bb98d17b8ef8
spmtitsolution <117553998+spmtitsolution@users.noreply.github.com>
2025-07-28 15:16:02 +0700

    change port

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -32,7 +32,6 @@ func CORSMiddleware() gin.HandlerFunc {
 			}
 		}
 
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
@@ -98,7 +97,7 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8082"
+		port = "3008"
 	}
 
 	srv := &http.Server{
